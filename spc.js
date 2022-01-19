8b7b1d181174b14284fec4c6649270929d551417
spcvinilos <98059043+spcvinilos@users.noreply.github.com>
2022-01-19 18:29:22 -0300

    Update spc.js

--- spc.js
+++ spc.js
@@ -1,28 +1,3 @@
-var millisecondsToWait = 2500;
-var firstClickRefresh = true;
-
-function refreshBtn(){
-    var i = 0
-    if(firstClickRefresh = true){
-        for(i;i<1;i++) {
-            (function () {
-              setTimeout(function(){
-                document.getElementById('btnRefresh').classList.add('rotate');
-                firstClickRefresh = false;
-              }, 100);
-            })(i);
-          }
-    }else{
-        for(i;i<1;i++) {
-            (function () {
-              setTimeout(function(){
-                document.getElementById('btnRefresh').classList.add('rotate');
-              }, millisecondsToWait);
-            })(i);
-          }
-    }
-}
-
 function showTime(){
 myDate = new Date();
 hours = myDate.getHours();
